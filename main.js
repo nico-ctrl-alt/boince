d67214073a02d02150d0927abd464a2acac1d8b2
nico-ctrl-alt <122402259+nico-ctrl-alt@users.noreply.github.com>
2023-09-09 15:51:55 -0400

    3 files modified

--- main.js
+++ main.js
@@ -2,23 +2,42 @@ import './style.css'
 
 import * as THREE from 'three';
 
-const scene = new THREE.scene();
+const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGL1Renderer({canvas: document.querySelector('bg'),});
+const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#bg'),});
 
-renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setPixelRatio(.2);
+renderer.setRenderTargetFramebuffer
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-const geometry = new THREE.IcosahedronGeometry(5, 3, 16, 100);
+const icogeo = new THREE.IcosahedronGeometry(5, 3, 16, 100);
 const material = new THREE.MeshToonMaterial( {color: 0x00ff00 });;
-const icos = new THREE.Mesh(geometry, material);
+const icos = new THREE.Mesh(icogeo, material);
 
-scene.add(icos)
+scene.add(icos);
+
+const color = 0xFFFFFF;
+const intensity = 1;
+const light = new THREE.AmbientLight(color, intensity);
+scene.add(light);
+
+const color1 = 0xFFFFFF;
+const intensity1 = 1;
+const light1 = new THREE.DirectionalLight(color1, intensity1);
+light1.position.set(0, 10, 0);
+light1.target.position.set(-5, 0, 0);
+scene.add(light1);
+scene.add(light1.target);
+
+const tomainmenu = false;
+let timer = 0;
+const scenetransition = 0;
 
 function animate() {
   requestAnimationFrame(animate);
@@ -26,6 +45,29 @@ function animate() {
   icos.rotation.x += .01;
 
   renderer.render(scene, camera);
+
+  if(timer > 0 && scenetransition == 0)
+  {
+    timer -= 1;
+    if(timer > 200)
+    {
+      icos.position.set(8,10,0);
+    }
+    if(timer > 100 && timer < 200)
+    {
+      icos.position.set(-4,-10,10);
+    }
+    if(timer > 0 && timer < 100)
+    {
+      icos.position.set(0,-2,20);
+    }
+  }
 }
 
-animate()
+animate();
+
+const button0 = document.getElementById('button0');
+button0.addEventListener('click', () => {change0();});
+button0.addEventListener('hover', () => {hover0();});
+
+
